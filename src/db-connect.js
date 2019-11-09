e5e45faf8c59486f8887a68bc2a2466bf0cb11c8
Chuan-Hsin-MacBookAir <49783514+bacacion@users.noreply.github.com>
2019-11-09 18:21:58 +0800

    Update db-connect.js

--- src/db-connect.js
+++ src/db-connect.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 const bluebird = require('bluebird');
 const db = mysql.createConnection({
-    host: '192.168.10.114',
+    host: 'localhost',
     user: 'root',
-    port: '3306',
     password: 'root',
+    port: '8889',
     database: 'shop'
 });
 db.connect();
