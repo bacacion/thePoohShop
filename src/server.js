926f7b41dc2dcd9ba177d2bb114c07226c51c97f
Chuan-Hsin-MacBookAir <49783514+bacacion@users.noreply.github.com>
2019-11-11 00:19:43 +0800

    SignIn ForgetPass Email(Not yet)

--- src/server.js
+++ src/server.js
@@ -9,8 +9,10 @@ const session = require('express-session');
 const moment = require('moment-timezone');
 var favicon = require('serve-favicon');
 const db = require(__dirname + '/db-connect');
+var nodemailer = require('nodemailer');
+const emailService = require(__dirname + '/email');
 
-// Middlewire
+// ---------------Middlewire---------------
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -25,17 +27,18 @@ app.use(session({
     }
 }));
 // session帶著走.
-app.use((req,res,next)=>{
-    if(req.session.loginUser){
+app.use((req, res, next) => {
+    if (req.session.loginUser) {
         res.locals.loginUser = req.session.loginUser;
-        }
+    }
     next();
 });
 
-// Route 
+// ---------------Route Start Here---------------
 app.get('/', (req, res) => {
     res.render('index');
 });
+
 app.get('/login', (req, res) => {
     let data = {
         loginUser: req.session.loginUser || "",
@@ -53,19 +56,23 @@ app.post('/login', (req, res) => {
         req.session.loginUser = req.body.email.split("@")[0];
         res.redirect('/');
     } else {
-        req.session.flashMsg='Incorrect email or password.'
+        req.session.flashMsg = 'Incorrect email or password.'
         res.redirect('/login');
     };
 
 });
-app.get('/logout', (req,res) => {
+
+app.get('/logout', (req, res) => {
     delete req.session.loginUser;
     delete res.locals.loginUser;
     res.render('index');
 });
+
 app.get('/sign-up', (req, res) => {
     res.render('sign-up');
 });
+
+//  TODO: 帳號是否有重複檢查.
 app.post('/sign-up', (req, res) => {
     // TODO: 資料檢查
     const output = {
@@ -75,22 +82,38 @@ app.post('/sign-up', (req, res) => {
         errorMsg: '',
         body: req.body
     };
+    // console.log("req from sign up");
+    if (!req.body.name || req.body.name.length < 2) {
+        output.code = 410;
+        output.errorMsg = '姓名請填兩個字以上';
+        return res.json(output);
+    }
+    const sqlDuplicate = "SELECT * FROM `client_data` WHERE email=?";
+    const sql = "INSERT INTO `client_data`(`email`, `password`, `client_name`, `birthday`, `address`, `fav_char`, `reg_time`, `class`) VALUES (?,?,?,?,?,'none', NOW(), 1)";
+    req.body.completeAddress = req.body.address + req.body.address2 + req.body.city + req.body.zip;
 
-    // if (!req.body.name || req.body.name.length < 2) {
-    //     output.code = 410;
-    //     output.errorMsg = '姓名請填兩個字以上';
-    //     return res.json(output);
-    // }
-
-    const sql = "INSERT INTO `client_data`(`email`, `password`, `client_name`, `birthday`, `address`, `reg_time`) VALUES (?,?,?,?,?,NOW())";
-    db.queryAsync(sql, [
+    db.queryAsync(sqlDuplicate, [
         req.body.email,
-        req.body.password,
-        req.body.name,
-        req.body.birthday,
-        req.body.address,
     ])
         .then(results => {
+            console.log("results.length is " + results.length)
+            console.log(results)
+            if (results.length < 1) {
+                output.code = 411;
+                output.errorMsg = 'Email duplicate!';
+                console.log("duplicate emm")
+                return res.json(output);
+            } else {
+                return db.queryAsync(sql, [
+                    req.body.email,
+                    req.body.password,
+                    req.body.name,
+                    req.body.birthday,
+                    req.body.completeAddress,
+                ]);
+            }})
+        .then(results => {
+            // console.log(results);
             output.results = results;
             if (results.affectedRows === 1) {
                 output.success = true;
@@ -106,12 +129,52 @@ app.post('/sign-up', (req, res) => {
         });
 
 });
+
 app.get('/forget-password', (req, res) => {
-    res.render('forget-password');
+    let data = {
+        forgetmail: req.session.forgetmail || "",
+        flashMsg: req.session.flashMsg
+    };
+    delete req.session.flashMsg;
+    res.render('forget-password', data);
+});
+app.post('/forget-password', (req, res) => {
+    const list = {
+        "aaa@gmail.com": "1234",
+        "bbb@gmail.com": "1234"
+    }
+    if (req.body.email && list[req.body.email]) {
+        req.session.forgetmail = list[req.body.email];
+        res.redirect('/forget-password');
+    } else {
+        req.session.flashMsg = 'Incorrect email.'
+        res.redirect('/forget-password');
+    };
 });
 
+app.get('/test', (req, res) => {
+    res.render('test');
+});
 
+app.get('/main', (req,res)=> {
+    res.render('main');
+});
 
+app.post('/test', (req, res) => {
+    const sql = "SELECT * FROM `client_data` WHERE email=?";
+    db.queryAsync(sql, [
+        // req.body.name,
+        req.body.text,
+    ])
+        .then(results => {
+            res.json(results);
+        })
+});
+// TODO: email function
+// app.get('/emailTest', (req,res)=>{
+//     emailService.send('dingnew01@gmail.com', 'aaaaa','bbbbb');
+//     redirect('/');
+// });
 
 // 404 要在 routes 的最後面
 app.use((req, res) => {
