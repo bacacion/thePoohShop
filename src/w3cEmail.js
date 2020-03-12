7a7e4ad31221a05db9d7e5926c17d5c7a1e2cf48
Chuan-Hsin <49783514+bacacion@users.noreply.github.com>
2020-03-12 11:45:04 +0800

    Update w3cEmail.js

--- src/w3cEmail.js
+++ src/w3cEmail.js
@@ -3,18 +3,12 @@ var nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'dingnew01@gmail.com',
-    pass: 'tcvlddrxsfirophr'
-    // pass: 'fhqcqbwnvkrsvdqe'
+    user: '### your email ###',
+    pass: '### your password ###'
+
   }
 });
 
-// var mailOptions = {
-//   from: 'dingnew01@gmail.com',
-//   to: 'b0227390004@gmail.com',
-//   subject: 'Sending Email using Node.js',
-//   text: 'That was easy!'
-// };
 function sendEmail(to,subject,text){
     const mailOptions={}
     mailOptions.to = to;
